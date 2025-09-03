c83e38d88f0ce468ef84c1adb30cf8c53c15337f
lagman-na-uzhin <nurtilek.rakhmtulla@narxoz.kz>
2025-09-03 14:54:53 +0500

    36

--- src/domain/organization/value-objects/contact.point.vo.ts
+++ src/domain/organization/value-objects/contact.point.vo.ts
@@ -88,7 +88,7 @@ export class ContactPoint {
                 break;
             case ContactPointType.WEBSITE:
                 // More comprehensive URL validation
-                isValid = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/[a-zA-Z0-9]+\.[^\s]{2,}|[a-zA-Z0-9]+\.[^\s]{2,})$/i.test(this._value);
+                isValid = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/[a-zA-Z0-9]+\.[^\s]{2,}|[a-zA-Z0-9]+\.[^\s]{2,})$/i.test(value);
                 break;
             default:
                 isValid = true;
